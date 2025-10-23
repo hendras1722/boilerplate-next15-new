7d875c3ce6f0b7dfed48ba176ef28ca94481c819
hendras1722 <muhsyahendraa1722@gmail.com>
2025-10-23 10:17:45 +0700

    feat: upgrade dependencies and Next.js version and fixing multiselect component

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,10 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      'react-hooks/rules-of-hooks': 'warn',
       'react-hooks/exhaustive-deps': 'off',
        "@typescript-eslint/no-explicit-any": ["off"]
     },
